8a1bea1898569ab2ba02c8c307b704110784845c
Avi <avishkardixita@gmail.com>
2026-01-27 01:10:10 +0530

    Billing App || added color filters in stock history and also changed the mobile view

--- src/app/pages/stock-book/stock-book.component.ts
+++ src/app/pages/stock-book/stock-book.component.ts
@@ -14,6 +14,8 @@ import { FormsModule } from '@angular/forms';
 export class StockBookComponent implements OnInit {
   stocks: any[] = [];
   history: any[] = [];
+  filteredHistory: any[] = [];
+  selectedColor = '';
 
   constructor(private stockService: StockService) {}
 
@@ -23,7 +25,25 @@ export class StockBookComponent implements OnInit {
 
   load() {
     this.stockService.getStock().subscribe(res => this.stocks = res);
-    this.stockService.getHistory().subscribe(res => this.history = res);
+
+    this.stockService.getHistory().subscribe(res => {
+      // newest first
+      this.history = res.sort(
+        (a: any, b: any) => new Date(b.date).getTime() - new Date(a.date).getTime()
+      );
+
+      this.filteredHistory = [...this.history];
+    });
+  }
+
+  applyFilter() {
+    if (!this.selectedColor) {
+      this.filteredHistory = [...this.history];
+    } else {
+      this.filteredHistory = this.history.filter(
+        h => h.color === this.selectedColor
+      );
+    }
   }
 
   save(stock: any) {
